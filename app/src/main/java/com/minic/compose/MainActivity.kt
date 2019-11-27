744e0e265b87db2164ca9a28fcab0c0043770a46
chenyy <674193357@qq.com>
2019-11-27 19:33:05 +0800

    UI微调

--- app/src/main/java/com/minic/compose/MainActivity.kt
+++ app/src/main/java/com/minic/compose/MainActivity.kt
@@ -9,14 +9,17 @@ import androidx.ui.core.Opacity
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.core.setContent
-import androidx.ui.engine.geometry.Shape
 import androidx.ui.foundation.Clickable
 import androidx.ui.foundation.DrawImage
 import androidx.ui.foundation.VerticalScroller
+import androidx.ui.foundation.shape.border.Border
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.graphics.Color
 import androidx.ui.layout.*
 import androidx.ui.material.Divider
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.ripple.Ripple
+import androidx.ui.material.surface.Surface
 import androidx.ui.text.style.TextOverflow
 import com.minic.compose.expansion.createImage
 import com.minic.compose.expansion.logD
@@ -42,6 +45,7 @@ class MainActivity : AppCompatActivity() {
             val articleTops = WARepository.articleTop().await()
             val articles = WARepository.article(1).await()
             withContext(Dispatchers.Main) {
+                articleTops.data.forEach { it.isTopping = true }
                 HomeArticles.mArticles.addAll(articleTops.data)
                 HomeArticles.mArticles.addAll(articles.data.datas)
             }
@@ -79,84 +83,113 @@ fun ArticleItems(articles: MutableList<ArticleData>) {
  */
 @Composable
 fun CreateArticleItem(articleData: ArticleData) {
-    Ripple(bounded = true) {
-        // 水波纹
-        Clickable(onClick = { logD("点击文章") }) {
-            Padding(padding = 15.dp) {
-                FlexColumn {
-                    inflexible {
-                        // 头像、作者、发布时间、公众号
-                        FlexRow(crossAxisAlignment = CrossAxisAlignment.Center) {
-                            // 头像
-                            inflexible {
-                                createImage(
-                                    type = PicassoLoader.ROUND,
-                                    url = App.INSTANCE.resources.getStringArray(R.array.author_imgs).random()
-                                )?.let { image ->
-                                    Container(width = 25.dp, height = 25.dp) {
-                                        DrawImage(image)
+    Surface(color = Color.White) {
+        Ripple(bounded = true) {
+            // 水波纹
+            Clickable(onClick = { logD("点击文章") }) {
+                Padding(padding = 15.dp) {
+                    FlexColumn {
+                        inflexible {
+                            // 头像、作者、发布时间、公众号
+                            FlexRow(crossAxisAlignment = CrossAxisAlignment.Center) {
+                                // 头像
+                                inflexible {
+                                    createImage(
+                                        type = PicassoLoader.ROUND,
+                                        url = App.INSTANCE.resources.getStringArray(R.array.author_imgs).random()
+                                    )?.let { image ->
+                                        Container(width = 25.dp, height = 25.dp) {
+                                            DrawImage(image)
+                                        }
+                                    }
+                                }
+                                // Margin间距
+                                inflexible {
+                                    WidthSpacer(width = 8.dp)
+                                }
+                                // 作者
+                                expanded(flex = 1f) {
+                                    Text(articleData.author, style = Tv12())
+                                }
+                                // 发布时间
+                                inflexible {
+                                    Text(articleData.niceDate, style = Tv13())
+                                }
+                            }
+                        }
+                        // 垂直间距
+                        inflexible {
+                            HeightSpacer(height = 10.dp)
+                        }
+                        expanded(1f) {
+                            FlexRow(
+                                mainAxisSize = LayoutSize.Expand
+                            ) {
+                                flexible(1f) {
+                                    FlexColumn(
+                                        crossAxisSize = LayoutSize.Expand,
+                                        mainAxisSize = LayoutSize.Expand
+                                    ) {
+                                        flexible(1f) {
+                                            // 标题
+                                            Padding(right = 10.dp) {
+                                                Text(
+                                                    text = articleData.title,
+                                                    maxLines = 2,
+                                                    overflow = TextOverflow.Ellipsis, style = Tv15()
+                                                )
+                                            }
+                                        }
+
+                                    }
+                                }
+                                // 右侧文章缩略图
+                                inflexible {
+                                    createImage(url = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2018939532,1617516463&fm=26&gp=0.jpg")?.let { image ->
+                                        Container(width = 80.dp, height = 80.dp) {
+                                            DrawImage(image)
+                                        }
                                     }
                                 }
                             }
-                            // Margin间距
-                            inflexible {
-                                WidthSpacer(width = 8.dp)
-                            }
-                            // 作者
-                            expanded(flex = 1f) {
-                                Text(articleData.author, style = Tv12())
-                            }
-                            // 发布时间
-                            inflexible {
-                                Text(articleData.niceDate, style = Tv13())
-                            }
                         }
-                    }
-                    // 垂直间距
-                    inflexible {
-                        HeightSpacer(height = 10.dp)
-                    }
-                    expanded(1f) {
-                        FlexRow(
-                            mainAxisSize = LayoutSize.Expand
-                        ) {
-                            flexible(1f) {
-                                FlexColumn(
-                                    crossAxisSize = LayoutSize.Expand,
-                                    mainAxisSize = LayoutSize.Expand
+                        inflexible {
+                            // 公众号
+                            Row(
+                                mainAxisAlignment = MainAxisAlignment.Center,
+                                modifier = ExpandedHeight,
+                                mainAxisSize = LayoutSize.Wrap,
+                                crossAxisSize = LayoutSize.Wrap
+                            ) {
+                                Surface(
+                                    shape = RoundedCornerShape(size = 2.dp),
+                                    border = Border(color = Color.Green, width = 1.dp)
                                 ) {
-                                    flexible(1f) {
-                                        // 标题
-                                        Padding(right = 10.dp) {
+                                    // 置顶
+                                    if (articleData.isTopping) {
+                                        Padding(
+                                            padding = EdgeInsets(
+                                                left = 3.dp,
+                                                right = 3.dp,
+                                                top = 1.dp,
+                                                bottom = 1.dp
+                                            )
+                                        ) {
                                             Text(
-                                                text = articleData.title,
-                                                maxLines = 2,
-                                                overflow = TextOverflow.Ellipsis, style = Tv15()
+                                                text = "置顶",
+                                                style = Tv11()
                                             )
                                         }
                                     }
-
                                 }
-                            }
-                            // 右侧文章缩略图
-                            inflexible {
-                                createImage(url = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2018939532,1617516463&fm=26&gp=0.jpg")?.let { image ->
-                                    Container(width = 80.dp, height = 80.dp) {
-                                        DrawImage(image)
-                                    }
+                                if (articleData.isTopping) {
+                                    WidthSpacer(width = 3.dp)
                                 }
+                                Text(
+                                    text = articleData.superChapterName + "·" + articleData.author,
+                                    style = Tv11()
+                                )
                             }
-                        }
-                    }
-                    inflexible {
-                        // 公众号
-                        Row(
-                            modifier = ExpandedHeight,
-                            mainAxisSize = LayoutSize.Wrap,
-                            crossAxisSize = LayoutSize.Wrap
-                        ) {
-                            Text(text = articleData.superChapterName,style = Tv11())
-                            Text(text = articleData.author,style = Tv11())
                         }
                     }
                 }
